298f01730087d1755746a9415683842fb33df198
DELL-PC\Andry Zirka <zirkaandry@gmail.com>
2017-10-31 15:07:53 +0200

    correced One

--- js/menu-vertical-root.js
+++ js/menu-vertical-root.js
@@ -1,17 +1,41 @@
 $(document).ready(function () {
 
 
+    var blackWrapper = $(".bg_layer"),
+        navMenu = $(".left-group-menu");
 
-            var blackWrapper = $(".bg_layer");
-            $(".menu-v-root-vis li").hover(function () {
-                blackWrapper.css({"display": "block"});
+    navMenu.hover(
+        function() {
 
-            }, function () {
-                blackWrapper.css({"display": "none"});
-            });
+            blackWrapper.css({"display": "block"})
+        }, function() {
+           blackWrapper.css({"display": "none"});
+        }
+    );
+
+//    ANCOR
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 500) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 'slow');
+            return false;
+        });
+    });
 
 
 
-    )};
+
+
+});
 
 
